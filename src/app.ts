93af3f82fc20983bf84d752c1f7eada28c8b3ca9
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-02-16 23:59:43 +0800

    Reset sqlite db daily every midnight

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { env } from '@app/config/environment';
 
+import { execSync } from 'child_process';
 import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
+import { scheduleJob } from 'node-schedule';
 
 import passport from 'passport';
 import './passport';
@@ -26,6 +28,13 @@ const startApp = async () => {
   app.use('/api/users', userRouter);
   app.use('/api/todos', todoRouter);
   app.use('/api', authRouter);
+
+  // Reset DB every midnight
+  scheduleJob('0 0 * * *', fireDate => {
+    // tslint:disable-next-line:no-console
+    console.info(`Execute scheduled DB reset @ ${fireDate.toString()}`);
+    execSync('npm run seed');
+  });
 
   app.listen(env.PORT, () => {
     // tslint:disable-next-line:no-console
